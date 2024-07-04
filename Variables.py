aaa99a83bf816d8810ee0c0c4017eccafebeb4e0
Exuled <salanding@yandex.ru>
2024-07-04 13:16:36 +0900

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+Homeworks = 12
+Number_of_hours = 1,5
+Course_name='Python'
+Time_for_one_exercise = 8
+print('Курс:',Course_name,', Всего задач', Homeworks,', Затрачено часов', Number_of_hours,', Среднее время выполнения', Time_for_one_exercise, 'минут')
