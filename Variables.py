d3a78e39ba28795a719d97c9e83a0f44e94cf1a1
Exuled <salanding@yandex.ru>
2024-07-04 23:36:15 +0900

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,5 +1,5 @@
 Homeworks = 12
 Number_of_hours = 1,5
 Course_name='Python'
-Time_for_one_exercise = 8
+Time_for_one_exercise = (12 / 1.5)
 print('Курс:',Course_name,', Всего задач', Homeworks,', Затрачено часов', Number_of_hours,', Среднее время выполнения', Time_for_one_exercise, 'минут')
